1ab431c975ec84da388366a1faa27d4c630e6c80
aman <kemalov2004@mail.ru>
2025-08-17 00:23:14 -0700

    task 025 is solving v1.3

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/main/ViewModels/ListViewModel.kt
@@ -0,0 +1,16 @@
+package ru.easycode.zerotoheroandroidtdd.main.ViewModels
+
+import ru.easycode.zerotoheroandroidtdd.main.BundleWrapper
+import ru.easycode.zerotoheroandroidtdd.main.ListLiveDataWrapper
+import ru.easycode.zerotoheroandroidtdd.main.Navigation
+
+class ListViewModel(private val liveDataWrapper: ListLiveDataWrapper, private val navigation: Navigation.Update) {
+    fun create() {
+    }
+    fun save(bundleWrapper: BundleWrapper.Save) {
+
+    }
+    fun restore(bundleWrapper: BundleWrapper.Restore) {
+
+    }
+}
